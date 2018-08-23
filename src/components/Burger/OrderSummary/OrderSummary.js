9c6c9eb29a3643ca2aac0162f0e5f5426122792c
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-08-23 19:59:54 +0700

    add total price in order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -20,6 +20,9 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
+      <p>
+        <strong>Total Price: USD {props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue for checkout?</p>
       <Button btnType="Danger" clicked={props.modalClosed}>CANCEL</Button>
       <Button btnType="Success" clicked={props.continue}>CONTINUE</Button>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -111,6 +111,7 @@ class BurgerBulder extends Component {
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}> 
           <OrderSummary 
             ingredients={this.state.ingredients} 
+            price={this.state.totalPrice}
             modalClosed={this.purchaseCancelHandler}
             continue={this.purchaseContinueHandler}></OrderSummary>
         </Modal>
