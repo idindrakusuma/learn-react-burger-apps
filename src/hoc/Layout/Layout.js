10440fe526bde404aa180c1b09338cbca80b22e5
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-08-24 21:41:01 +0700

    restructure folder project

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 /* own component */
-import Aux from '../../hoc/Aux';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideBar from '../Navigation/SideDrawer/SideDrawer';
+import Aux from '../Aux/Aux';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideBar from '../../components/Navigation/SideDrawer/SideDrawer';
 /* style css */
 import classes from './Layout.css'
 
