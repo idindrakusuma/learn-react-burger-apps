767d18ef0829c31609e212c4d8cd451aba3357a4
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-10-11 15:28:00 +0700

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -9,8 +10,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/" exact component={BurgerBuilder}/>
+            <Route path="/checkout" component={Checkout}/>
+          </Switch>
         </Layout>
       </div>
     );
