136a056730560b08ace0f38db89f9d0b225cdbc8
idindrakusuma <indrakusuma.udinus@gmail.com>
2018-10-11 15:58:56 +0700

    using query route

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -134,7 +134,16 @@ class BurgerBulder extends Component {
     //     console.log(err)
     //     this.setState({ loading: false, purchasing: false})
     //   })
-    this.props.history.push('/checkout');
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    }
+    const queryString = queryParams.join('&');
+    
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString,
+    });
   }
 
   render () {
